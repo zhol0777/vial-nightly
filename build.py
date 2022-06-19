d08598185873a950030f6f442560ff12b365fda3
zhol internet <bigman@zholworld.gov>
2022-06-18 22:31:14 -0700

    bug fixes

--- build.py
+++ build.py
@@ -2,18 +2,16 @@
 
 '''
 Mostly just builds vial firmware blobs and does some organizing and touching up index.html
-and build.sh should handle the rest
-# TODO: make build.py handle all the stuff build.sh still has left
 '''
 
 import argparse
 import glob
 import logging
-import os
 import subprocess
 import shutil
+import sys
 
-from pathlib import Path
+from pathlib import Path, PurePath
 from threading import Thread
 
 from ansi2html import Ansi2HTMLConverter
@@ -24,6 +22,7 @@ QMK_FIRMWARE_DIR = '/qmk_firmware'
 QMK_DOCKER_IMAGE = 'qmkfm/base_container'
 PAGE_HEADER = 'vial-qmk nightly'
 FIRMWARE_TAR = 'vial-files.tar'
+COMMIT_ID_FILE = '.commit_id'
 PAGE_CHAR_WIDTH = 72
 
 logging.basicConfig(level=logging.DEBUG)
@@ -45,12 +44,13 @@ def docker_run_cmd(args: argparse.Namespace, container_id: str, cmd: str, line: 
     try:
         subprocess_cmd = f'docker {cmd} {container_id} {line}'
         log.info('Running: %s', subprocess_cmd)
-        subprocess.run(subprocess_cmd, shell=True, stdout=subprocess.DEVNULL, check=True)
+        subprocess.run(subprocess_cmd, shell=True, stdout=subprocess.DEVNULL,
+                       stderr=subprocess.DEVNULL, check=True)
     except subprocess.CalledProcessError:
         log.exception("Exception raised by failure to run command in docker")
         if not args.debug:
             close_containers(container_id)
-        exit(1)
+        sys.exit(1)
 
 
 def docker_cmd_stdout(container_id: str, line: str):
@@ -64,8 +64,24 @@ def docker_cmd_stdout(container_id: str, line: str):
 
 def close_containers(container_id: str):
     '''Close qmkfm/basecontainer docker containers when needed'''
+    log.info("Closing docker containers...")
     subprocess.run(f'docker stop {container_id}', shell=True, check=True)
     subprocess.run(f'docker container rm {container_id}', shell=True, check=True)
+
+
+def files_still_fresh(git_commit_id: str, cwd: Path) -> bool:
+    '''
+    Write a little file with the commit ID of HEAD for git repo,
+    and return True if it matches repo on pulling from git most recently,
+    and save us work of recompiling files all the way over again
+    '''
+    old_commit_id_file = Path(cwd, COMMIT_ID_FILE)
+    if old_commit_id_file.exists():
+        old_commit_id = old_commit_id_file.read_text(encoding='utf-8')
+        if old_commit_id == git_commit_id:
+            return True
+    old_commit_id_file.write_text(git_commit_id, encoding='utf-8')
+    return False
 
 
 def main():
@@ -83,13 +99,18 @@ def main():
 
     if args.close_docker_containers:
         close_containers('vial')
-        exit(0)
+        sys.exit(0)
 
     cwd = Path.cwd()
-    # vial_dir = os.path.join(cwd, 'vial')
     vial_dir = Path(cwd, 'vial')
 
-    container_id = prepare_container(args)
+    container_id, git_commit_id = prepare_container(args, cwd)
+
+    git_commit_id = docker_cmd_stdout(container_id, 'git rev-parse HEAD').strip()
+    if files_still_fresh(git_commit_id, cwd):
+        log.info("Files are still fresh! Skipping compilation!")
+        close_containers(container_id)
+        sys.exit(0)
 
     total_build_output = compile_within_container(args, container_id)
 
@@ -100,23 +121,20 @@ def main():
     # prepare serving for creation/refresh
     Path('vial').mkdir(exist_ok=True)
 
-    for root, _, stale_files in os.walk(vial_dir):
-        for stale_file in stale_files:
-            os.remove(os.path.join(root, stale_file))
+    # this has no subdirectories, so no need to recurse
+    for dir_file in vial_dir.iterdir():
+        dir_file.unlink()
     template_path = Path(cwd, 'templates', 'template.html.jinja')
-    # with open(template_path, 'r', encoding='utf8') as template_file:
-    #     html_template = Template(template_file.read(), trim_blocks=True, lstrip_blocks=True)
     html_template = Template(template_path.read_text(encoding='utf8'),
                              trim_blocks=True, lstrip_blocks=True)
 
     log.info("Untar tarball")
     subprocess.run(f'tar -xvf {FIRMWARE_TAR}', shell=True, stdout=subprocess.DEVNULL, check=True)
-    os.remove(os.path.join(cwd, FIRMWARE_TAR))
+    Path(cwd, FIRMWARE_TAR).unlink()
 
     fw_files = []
-    for _, _, files in os.walk(vial_dir):
-        for fw_file in files:
-            fw_files.append(fw_file)
+    for fw_file in vial_dir.iterdir():
+        fw_files.append(fw_file.name)
     fw_files.sort()
 
     git_log = docker_cmd_stdout(container_id, 'git log --decorate')
@@ -124,7 +142,7 @@ def main():
     git_log = git_log.replace('>', ')')
     template_data = {
         'page_header': PAGE_HEADER,
-        'git_commit_id': docker_cmd_stdout(container_id, 'git rev-parse HEAD').strip(),
+        'git_commit_id': git_commit_id,
         'build_time': subprocess.check_output("date", shell=True, encoding='utf8'),
         'git_log': git_log,
         'builds': [],
@@ -134,7 +152,7 @@ def main():
     open_threads: list[Thread] = []
     for line in total_build_output.split('\n'):
         new_thread = Thread(target=process_build_output,
-                            args=(line, vial_dir, container_id, template_data, fw_files))
+                            args=(line, vial_dir, container_id, template_data))
         open_threads.append(new_thread)
         new_thread.start()
 
@@ -143,17 +161,18 @@ def main():
 
     template_data['builds'] = sorted(template_data['builds'], key=lambda d: d['sort_line'])
 
-    index_html_path = os.path.join(vial_dir, 'index.html')
+    index_html_path = Path(vial_dir, 'index.html')
     with open(index_html_path, 'w', encoding='utf8') as index_html:
         index_html.write(html_template.render(template_data))
 
-    shutil.copyfile(os.path.join(cwd, 'favicon.ico'),
-                    os.path.join(cwd, 'vial', 'favicon.ico'))
+    shutil.copyfile(Path(cwd, 'favicon.ico'),
+                    Path(vial_dir, 'favicon.ico'))
 
     if not args.debug:
         close_containers(container_id)
 
-def prepare_container(args: argparse.ArgumentParser) -> str:
+
+def prepare_container(args: argparse.Namespace, cwd: Path) -> (str, str):
     '''Spin up docker container from qmkfm/base_container and return container ID'''
     if args.debug:
         container_id = 'vial'
@@ -165,17 +184,22 @@ def prepare_container(args: argparse.ArgumentParser) -> str:
             container_id = subprocess.check_output(create_container_command,
                                                    shell=True, encoding='utf8').strip()
         except subprocess.CalledProcessError:
-            exit(125)
+            sys.exit(125)
 
     if not args.debug:
         docker_run_cmd(args, container_id, 'exec',
                        f'git clone --depth=5 {VIAL_GIT_URL} {QMK_FIRMWARE_DIR}')
+        git_commit_id = docker_cmd_stdout(container_id, 'git rev-parse HEAD').strip()
+        if files_still_fresh(git_commit_id, cwd):
+            log.info("Files are still fresh! Skipping compilation!")
+            close_containers(container_id)
+            sys.exit(0)
         docker_run_cmd(args, container_id, 'exec', 'make git-submodule')
 
-    return container_id
+    return container_id, git_commit_id
 
 
-def compile_within_container(args: argparse.ArgumentParser, container_id: str) -> str:
+def compile_within_container(args: argparse.Namespace, container_id: str) -> str:
     '''Run commands to compile all vial fw within container provided'''
     # thank you piginzoo for showing me what i did wrong here
     total_build_output = docker_cmd_stdout(container_id, 'qmk multibuild -j`nproc` -km vial')
@@ -212,21 +236,21 @@ def process_build_output(line: str, vial_dir: str, container_id: str, template_d
     elif '[ERRORS]' in line:
         # delete bad firmware, since it is still there when it is too large
         implied_firmware_name = line.split()[1].replace(':', '_').replace('/', '_')
-        implied_firmware_glob = glob.glob(f'{os.path.join(vial_dir, implied_firmware_name)}.*')
+        implied_firmware_glob = glob.glob(f'{Path(vial_dir, implied_firmware_name)}.*')
         for file_path in implied_firmware_glob:
-            os.remove(file_path)
+            Path(file_path).unlink()
             try:
-                template_data['fw_files'].remove(os.path.basename(file_path))
+                template_data['fw_files'].remove(PurePath(file_path).name)
             except ValueError:
-                log.error("Could not remove %s from fw_files list", os.path.basename(file_path))
+                log.error("Could not remove %s from fw_files list", PurePath(file_path).name)
 
         # document failure
         errored_board = line.split()[1].split(':')[0]
         individual_build_output = \
             docker_cmd_stdout(container_id, f'qmk compile -kb {errored_board} -km vial')
         html = conv.convert(individual_build_output)
-        with open(os.path.join(vial_dir, f'{implied_firmware_name}.html'),
-                                'w', encoding="utf-8") as error_file:
+        with open(Path(vial_dir, f'{implied_firmware_name}.html'),
+                  'w', encoding="utf-8") as error_file:
             error_file.write(html)
 
         build['errors'] = True
